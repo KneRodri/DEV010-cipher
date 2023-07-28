e9c7339b6831107cc5677c3b53d43e97e003d3b7
unknown <knelia1989@gmail.com>
2023-07-28 17:29:39 -0500

    Está realizado todos los cambios

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,39 @@
 const cipher = {
-  // ...
+  encode: function (offset, text) {
+    let result = '';
+    if ((Number.isInteger(offset)&& offset > 0) || typeof text === "string") {
+
+      for (let i = 0; i < text.length; i++) {
+        let char = text[i];
+        if (char.match(
+          /[A-Z]/)) {
+          char = String.fromCharCode((text.charCodeAt(i) - 65 + offset) % 26 + 65);
+        }
+        result += char;
+      }
+      return result;
+    }
+    else { throw new TypeError("error");
+
+    }
+  },
+  decode: function(offset, text) {
+    if((Number.isInteger(offset)&& offset > 0) || typeof text === "string"){
+      let result = '';
+      for (let i = 0; i < text.length; i++) {
+        let char = text[i];
+        if (char.match(/[A-Z]/)) {
+          char = String.fromCharCode((text.charCodeAt(i) - 65 - offset % 26 + 26 + 26) % 26 + 65);
+        }
+        result += char;
+      }
+      return result;
+    }
+    else { throw new TypeError("otro error");
+
+    }
+  },
 };
 
 export default cipher;
+
--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+//Para obtener elementos del DOM
+const inputElement = document.getElementById('input');
+const btnInsertar = document.getElementById('btnInsertar');
+const textAreaElement = document.getElementById('texto');
+const btnEncode = document.getElementById('btnEncode');
+const btnDecode = document.getElementById('btnDecode');
+
+function capturaIngreso() {
+  const offset = parseInt(inputElement.value);  
+  if (!isNaN(offset)) { //Revisa si el valor es un número válido NaN: not a number
+    cipher.offset = offset;//indica el desplazamiento
+  }
+}
+
+//Función para cifrar el texto del textarea
+function encodeText() {
+//Toma un valor numérico del input, lo convierte en entero con parse y lo almacena en la variable
+  const offset = parseInt(inputElement.value);
+  const text = textAreaElement.value;//Para obtener el contenido a manipular
+  const encryptedText = cipher.encode(offset, text);//trae desde cipher
+  textAreaElement.value = encryptedText;
+}
+
+function decodeText() {
+  const offset = parseInt(inputElement.value);
+  const text = textAreaElement.value;
+  const decryptedText = cipher.decode(offset, text);
+  textAreaElement.value = decryptedText;
+}
+
+
+btnInsertar.addEventListener('click', capturaIngreso);
+btnEncode.addEventListener('click', encodeText);
+btnDecode.addEventListener('click', decodeText);
